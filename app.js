9b644ec0957d8553c591be7de5859a5545c8aaf8
amymaraisane <amy.maraisane@gmail.com>
2019-05-24 10:30:09 -0400

    display:flex and justify-content: text-center are not working, may come back to this issue later

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ mongoose.set('useFindAndModify', false);
 
 app.use(bodyParser.urlencoded({ extended: true }));;
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 app.use(methodOverride("_method"));
 app.use(expressSanitizer());
 
